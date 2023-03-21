74c77fce7d84058d4db0119ea5c03aa71a13e6f5
Jashan_workspace <Jashan@aws_workspace>
2023-03-21 14:24:05 -0700

    Exercise 1 correcting spelling

--- src/game_lib/Game.cpp
+++ src/game_lib/Game.cpp
@@ -136,7 +136,7 @@ bool Game::handleCorrectAnswer ()
   else
   {
     // preserve existing bug behavior
-    string correct = inPenaltyBox[currentPlayer] ? "correct" : "corrent";
+    string correct = inPenaltyBox[currentPlayer] ? "correct" : "correct";
     cout << "Answer was " << correct << "!!!!" << endl;
 
     purses[currentPlayer]++;
